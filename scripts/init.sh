7db4fc2a9c87965fa97896175f9c13ff27599302
Eryk Napierała <eryk.piast@gmail.com>
2016-01-01 20:24:16 +0000

    remove git directory on init

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,13 +5,15 @@ USER_NAME=${2:-erykpiast}
 
 echo "Initializing package $PACKAGE_NAME for user $USER_NAME..."
 
+rm -rf '.git'
+
 FILES=`find . ! -wholename '*/node_modules/*' ! -wholename '*/.git/*' ! -wholename '*/dist/*' ! -name 'init.sh' -type f  | xargs readlink -e`
 
 for F in $FILES
 do
   echo "Processing file $F..."
 	sed -i "s/npm-package-seed/$PACKAGE_NAME/g" "$F"
-	sed -i "s/erykpiast/$USER_NAME/g" "$F"
+	sed -i "s/erykpiast/$USEqqR_NAME/g" "$F"
 done
 
 echo "Package $PACKAGE_NAME initialized!"
